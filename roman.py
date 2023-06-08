fda8014863fe00f10395398d9188a6a1f1a8c6fe
Dave Wade-Stein <dave@developintelligence.com>
2023-06-08 10:02:15 -0600

    More changes

---
+++ roman.py
@@ -0,0 +1,19 @@
+converter = {
+    'M': 1000,
+    'D': 500,
+    'C': 100,
+    'L': 50,
+    'X': 10, 
+    'V': 5,
+    'I': 1,
+}
+
+def roman_to_arabic(numeral):
+    """Return Hindu-Arabic equivalent of Roman numeral."""
+    total = 0
+    for digit in numeral:
+        if digit not in converter:
+            raise ValueError('Bad Roman digit!')
+        total += converter[digit]
+
+    return total
